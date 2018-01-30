e713cecb0f12eb368d62ebd1f47d0958a3d226db
lidandan <lidandan@kunteng.org>
2018-01-30 10:23:02 +0800

    change sidebar 0130

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -20,110 +20,178 @@
 </template>
 
 <script>
+    import global_ from 'components/common/Global';
     export default {
         data() {
             return {
-                items: [
-                    {
-                        icon: 'el-icon-menu',
-                        index: '2',
-                        title: '渠道管理',
-                        subs: [
-                            {
-                                index: 'basetable',
-                                title: '渠道列表'
-                            },
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-upload',
-                        index: '3',
-                        title: '批量执行任务',
-                        subs: [
-                            {
-                                index: 'setpush',
-                                title: '发起推送'
-                            },
-                            {
-                                index: 'pushresult',
-                                title: '推送结果'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-setting',
-                        index: '4',
-                        title: '设备管理',
-                        subs: [
-                            {
-                                index: 'devstatus',
-                                title: '设备状态'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-star-on',
-                        index: '5',
-                        title: 'ROM管理',
-                        subs: [
-                            {
-                                index: 'rommanage',
-                                title: 'ROM列表'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-date',
-                        index: '6',
-                        title: '插件管理',
-                        subs: [
-                            {
-                                index: 'pluginmanage',
-                                title: '插件列表'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-document',
-                        index: '7',
-                        title: '脚本管理',
-                        subs: [
-                            {
-                                index: 'scriptmanage',
-                                title: '脚本列表'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-share',
-                        index: '8',
-                        title: '设备型号管理',
-                        subs: [
-                            {
-                                index: 'modelmanage',
-                                title: '设备型号列表'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-star-off',
-                        index: '9',
-                        title: '辅助工具',
-                        subs: [
-                            {
-                                index: 'sysinfo',
-                                title: '路由SYSINFO查询'
-                            }
-                        ]
-                    }
-                ]
+                items: []
             }
         },
         computed:{
             onRoutes(){
                 return this.$route.path.replace('/','');
             }
-        }
+        },
+        methods:{
+            getUser: function(){
+                var self = this;
+                self.$axios.post(global_.baseUrl+'/admin/info').then(function(res){
+                    if(res.data.ret_code == 0){
+                        self.curUser = res.data.ret_msg;
+                        if(self.curUser == '1'){//普通管理员
+                            self.items = [
+                                {
+                                    icon: 'el-icon-upload',
+                                    index: '3',
+                                    title: '批量执行任务',
+                                    subs: [
+                                        {
+                                            index: 'setpush',
+                                            title: '发起推送'
+                                        },
+                                        {
+                                            index: 'pushresult',
+                                            title: '推送结果'
+                                        }
+                                    ]
+                                },
+                                {
+                                    icon: 'el-icon-setting',
+                                    index: '4',
+                                    title: '设备管理',
+                                    subs: [
+                                        {
+                                            index: 'devstatus',
+                                            title: '设备状态'
+                                        }
+                                    ]
+                                },
+                                {
+                                    icon: 'el-icon-share',
+                                    index: '8',
+                                    title: '设备型号管理',
+                                    subs: [
+                                        {
+                                            index: 'modelmanage',
+                                            title: '设备型号列表'
+                                        }
+                                    ]
+                                },
+                                {
+                                    icon: 'el-icon-star-off',
+                                    index: '9',
+                                    title: '辅助工具',
+                                    subs: [
+                                        {
+                                            index: 'sysinfo',
+                                            title: '路由SYSINFO查询'
+                                        }
+                                    ]
+                                }
+                            ]
+                        }else{
+                            self.items = [
+                                {
+                                    icon: 'el-icon-menu',
+                                    index: '2',
+                                    title: '渠道管理',
+                                    subs: [
+                                        {
+                                            index: 'basetable',
+                                            title: '渠道列表'
+                                        },
+                                    ]
+                                },
+                                {
+                                    icon: 'el-icon-upload',
+                                    index: '3',
+                                    title: '批量执行任务',
+                                    subs: [
+                                        {
+                                            index: 'setpush',
+                                            title: '发起推送'
+                                        },
+                                        {
+                                            index: 'pushresult',
+                                            title: '推送结果'
+                                        }
+                                    ]
+                                },
+                                {
+                                    icon: 'el-icon-setting',
+                                    index: '4',
+                                    title: '设备管理',
+                                    subs: [
+                                        {
+                                            index: 'devstatus',
+                                            title: '设备状态'
+                                        }
+                                    ]
+                                },
+                                {
+                                    icon: 'el-icon-star-on',
+                                    index: '5',
+                                    title: 'ROM管理',
+                                    subs: [
+                                        {
+                                            index: 'rommanage',
+                                            title: 'ROM列表'
+                                        }
+                                    ]
+                                },
+                                {
+                                    icon: 'el-icon-date',
+                                    index: '6',
+                                    title: '插件管理',
+                                    subs: [
+                                        {
+                                            index: 'pluginmanage',
+                                            title: '插件列表'
+                                        }
+                                    ]
+                                },
+                                {
+                                    icon: 'el-icon-document',
+                                    index: '7',
+                                    title: '脚本管理',
+                                    subs: [
+                                        {
+                                            index: 'scriptmanage',
+                                            title: '脚本列表'
+                                        }
+                                    ]
+                                },
+                                {
+                                    icon: 'el-icon-share',
+                                    index: '8',
+                                    title: '设备型号管理',
+                                    subs: [
+                                        {
+                                            index: 'modelmanage',
+                                            title: '设备型号列表'
+                                        }
+                                    ]
+                                },
+                                {
+                                    icon: 'el-icon-star-off',
+                                    index: '9',
+                                    title: '辅助工具',
+                                    subs: [
+                                        {
+                                            index: 'sysinfo',
+                                            title: '路由SYSINFO查询'
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    }
+                })
+            },
+        },
+        mounted: function(){
+            this.getUser();
+        },
     }
 </script>
 
